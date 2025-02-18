22e2239883cb0c29239021b799acf698dd15e096
Georg Maißer <georg.maisser@wunderbyte.at>
2025-02-18 13:49:06 +0100

    Version (2025021800)

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->release = 'v0.0.11';
-$plugin->version   = 2025021301;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2025021800;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051100;        // Requires this Moodle version.
 $plugin->component = 'paygw_mpay24';       // Full name of the plugin (used for diagnostics).
