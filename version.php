3b7b18f502d6e6335a7ed67626c0d90601b2863f
Mona <mona.shakibapour@wunderbyte.at>
2025-05-28 12:28:44 +0200

    Switch to reusable workflow

--- version.php
+++ version.php
@@ -29,3 +29,4 @@ $plugin->release = 'v0.0.11';
 $plugin->version   = 2025021800;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051100;        // Requires this Moodle version.
 $plugin->component = 'paygw_mpay24';       // Full name of the plugin (used for diagnostics).
+$plugin->supported = [401, 405];
